90a6fbba38e3ad0f1fc636322bbd9d5488bbcff8
Banashree81 <62277816+Banashree81@users.noreply.github.com>
2021-05-04 17:49:23 +0530

    Update food.js

--- food.js
+++ food.js
@@ -1,26 +1,30 @@
 class Food{
     constructor(){
-        this.foodStock = foodS;       
+        this.foodStock = 0;       
         this.image = loadImage('images/Milk.png');
     }
 
-   /* getFoodStock(){}    
+    getFoodStock(){}
 
-    updateFoodStock(){}
+
+    /*updateFoodStock(){}
     
     deductFood(){}*/
 
     display(){
         var x = 80, y = 100;
 
-        console.log(this.foodStock)
+        //console.log(this.foodStock)
 
        // imageMode(CENTER)
        if(this.foodStock===0){
         image(this.image, 150,220,70,70);
        }else  if(this.foodStock!== 0){
+       
             for (let index = 0; index < this.foodStock; index++) {
+               
                 if(index%10===0){
+                   
                     x = 80;
                     y = y+50;
                 }
